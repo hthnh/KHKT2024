0078f6beb8e1f5d02dc5f93e182c35f6d4603af5
hthnh <haithinh09032006@gmail.com>
2024-05-16 14:33:34 +0700

    h

--- vision.py
+++ vision.py
@@ -86,6 +86,8 @@ for action in actions:
             os.makedirs(os.path.join(DATA_PATH, action, str(dirmax+sequence)))
         except:
             pass
+
+
 cap = cv2.VideoCapture(0)
 # Set mediapipe model 
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
