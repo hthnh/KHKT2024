d3400028b1c843508cea116867ec80a55dd366d4
hthnh <haithinh09032006@gmail.com>
2024-05-16 14:47:38 +0700

    c

--- vision.py
+++ vision.py
@@ -37,38 +37,38 @@ def draw_styled_landmarks(image, results):
 mp_holistic.POSE_CONNECTIONS
 
 
-cap = cv2.VideoCapture(0)
-with mp_holistic.Holistic(min_detection_confidence=0.5,min_tracking_confidence=0.5)as holistic:
-    while cap.isOpened():
-        ret, frame = cap.read()
-        image, results = md(frame, holistic )
-        draw_styled_landmarks(image,results)
-        cv2.imshow('OpenCV Feed', image)
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
-    cap.release()
-    cv2.destroyAllWindows()
-plt.imshow(cv2.cvtColor(frame,cv2.Color_BGR2RGB))
-pose = []
-for res in results.pose_landmarks.landmark:
-    test = np.array([res.x, res.y, res.z, res.visibility])
-    pose.append(test)
-pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(132)
-face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(1404)
-lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
-rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
-face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() 
+# cap = cv2.VideoCapture(0)
+# with mp_holistic.Holistic(min_detection_confidence=0.5,min_tracking_confidence=0.5)as holistic:
+#     while cap.isOpened():
+#         ret, frame = cap.read()
+#         image, results = md(frame, holistic )
+#         draw_styled_landmarks(image,results)
+#         cv2.imshow('OpenCV Feed', image)
+#         if cv2.waitKey(10) & 0xFF == ord('q'):
+#             break
+#     cap.release()
+#     cv2.destroyAllWindows()
+# plt.imshow(cv2.cvtColor(frame,cv2.Color_BGR2RGB))
+# pose = []
+# for res in results.pose_landmarks.landmark:
+#     test = np.array([res.x, res.y, res.z, res.visibility])
+#     pose.append(test)
+# pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(132)
+# face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(1404)
+# lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+# rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+# face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() 
 
-def extract_keypoints(results):
-    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
-    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
-    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
-    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
-    return np.concatenate([pose, face, lh, rh])
-result_test = extract_keypoints(results)
+# def extract_keypoints(results):
+#     pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
+#     face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
+#     lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+#     rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+#     return np.concatenate([pose, face, lh, rh])
+# result_test = extract_keypoints(results)
 
-np.save('0', result_test)
-np.load('0.npy')
+# # np.save('0', result_test)
+# np.load('0.npy')
 # Path for exported data, numpy arrays
 DATA_PATH = os.path.join('MP_Data') 
 # Actions that we try to detect
