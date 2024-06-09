78a36ff60de955c6464ef0daad8eeb5226890b25
hthnh <haithinh09032006@gmail.com>
2024-06-09 15:32:05 +0700

    j

--- vision.py
+++ vision.py
@@ -19,18 +19,14 @@ actions = np.array(['A','B'])
 
 
 model = Sequential()
-
-model.add(LSTM(256, return_sequences=True, activation='relu', input_shape=(15,1662)))
-model.add(LSTM(512, return_sequences=True, activation='relu'))
-model.add(LSTM(256, return_sequences=False, activation='relu'))
-model.add(Dense(256, activation='relu'))
-model.add(Dense(128, activation='relu'))
+mmodel = Sequential()
+model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(15,1662)))
+model.add(LSTM(128, return_sequences=True, activation='relu'))
+model.add(LSTM(64, return_sequences=False, activation='relu'))
 model.add(Dense(64, activation='relu'))
 model.add(Dense(32, activation='relu'))
 model.add(Dense(actions.shape[0], activation='softmax'))
 model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
-
-
 model.load_weights('action.keras')
 
 
@@ -104,17 +100,16 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
         ret, frame = cap.read()
 
         image, results = mediapipe_detection(frame, holistic)
-        print(results)
         
         draw_styled_landmarks(image, results)
         
         keypoints = extract_keypoints(results)
         sequence.append(keypoints)
         sequence = sequence[-30:]
-        
+        print(sentence)
         if len(sequence) == 30:
             res = model.predict(np.expand_dims(sequence, axis=0))[0]
-            print(actions[np.argmax(res)])
+            print(res)
             
             
             if res[np.argmax(res)] > threshold: 
