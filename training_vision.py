97cdba4897440cf5bdfb9f290794d54f6b609556
hthnh <haithinh09032006@gmail.com>
2024-06-09 16:33:57 +0700

    d

--- training_vision.py
+++ training_vision.py
@@ -42,7 +42,7 @@ model.add(Dense(64, activation='relu'))
 model.add(Dense(32, activation='relu'))
 model.add(Dense(actions.shape[0], activation='softmax'))
 model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
-model.fit(X_train, y_train, epochs=1000, callbacks=[tb_callback])
+model.fit(X_train, y_train, epochs=2000, callbacks=[tb_callback])
 
 model.save("action.keras")
 
